65132ce8f928f01ab3e73366303f40940f213894
Ming Ye <ymqyit@gmail.com>
2021-04-20 20:37:34 +0800

    init examples

---
+++ examples/cra/src/App.js
@@ -0,0 +1,12 @@
+// import { SliderComponent } from 'react-easy-slider';
+
+function App() {
+  return (
+    <div className="App">
+      11
+      {/* <SliderComponent /> */}
+    </div>
+  );
+}
+
+export default App;
