84ba047f9811dcb39a7fd114d2fe066fdb4c26e4
Ming Ye <ymqyit@gmail.com>
2021-04-22 14:09:13 +0800

    update SliderComponent

--- src/SliderComponent.tsx
+++ src/SliderComponent.tsx
@@ -1,22 +1,17 @@
-import React from 'react';
+import React, { useRef, FC, ReactNode } from 'react';
 import './index.less';
 
-const SliderComponent = (props) => {
+interface Props {
+  children?: ReactNode;
+}
+
+const SliderComponent: FC<Props> = (props) => {
+  const containerRef = useRef(null);
+
   return (
-    <div id="my-slider" className="slider-list">
+    <div ref={containerRef} id="my-slider" className="slider-list">
       <ul>
-        <li className="slider-list__item--selected">
-          <img src="https://p5.ssl.qhimg.com/t0119c74624763dd070.png"/>
-        </li>
-        <li className="slider-list__item">
-          <img src="https://p4.ssl.qhimg.com/t01adbe3351db853eb3.jpg"/>
-        </li>
-        <li className="slider-list__item">
-          <img src="https://p2.ssl.qhimg.com/t01645cd5ba0c3b60cb.jpg"/>
-        </li>
-        <li className="slider-list__item">
-          <img src="https://p4.ssl.qhimg.com/t01331ac159b58f5478.jpg"/>
-        </li>
+        { props.children }
       </ul>
     </div>
   )
